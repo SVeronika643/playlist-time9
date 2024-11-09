d911197868f355d8266315b0c9e85929aea9f688
SVeronika643 <snixon643@gmail.com>
2024-11-09 17:41:24 +0300

    проверяем подходящие песни

--- main.py
+++ main.py
@@ -73,12 +73,7 @@ def playingtime(playlist, n):
             time += playlist[i]
     elif type(playlist) == list and len(playlist) == 2 and type(playlist[0]) == type(playlist[1]) == tuple:
         playlist_dict = dict()
-        for i in range(len(playlist[0])):
-            playlist_dict[playlist[0][i]] = playlist[1][i]
-        names = sample(list(playlist_dict.keys()), n)
-        for i in names:
-            time += playlist_dict[i]
-    return time
+        
 
 print(playingtime(playlist_f,3))
 
